0886fb283520d5bb7eb8e0a1464895410db06834
LazyGon <yosihiro5225@gmail.com>
2019-03-01 01:24:59 +0900

    add Inperatrix_permprefix and fix issue about tps limit (no more TPS 27)

--- src/main/java/com/github/akaregi/imperatrix/Imperatrix.java
+++ src/main/java/com/github/akaregi/imperatrix/Imperatrix.java
@@ -20,6 +20,8 @@ package com.github.akaregi.imperatrix;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 import lombok.Getter;
 
@@ -69,10 +71,26 @@ public class Imperatrix extends PlaceholderExpansion {
      * @return TPS value as String if success, or empty String.
      */
     public String onPlaceholderRequest(Player player, String identifier) {
-        if (!(identifier.startsWith("tps"))) return "";
 
+        if (identifier.toLowerCase().startsWith("permprefix_")) return this.getPrefix(player, identifier);
+
+        if (identifier.equalsIgnoreCase("tps")) return this.getTps();
+        
+        return "";
+    }
+    /**
+     * Get result
+     * 
+     * @author akaregi
+     * @since 1.0.0-SNAPSHOT
+     * 
+     * @param server Minecraft Server, from NMS invocation.
+     * 
+     * @return TPS (String) 20.0 or lower.
+     */
+    public String getTps(){
         try {
-            return String.valueOf(round(getTPS(server)[0]));
+            return String.valueOf(Math.min(round(getTPS(server)[0]), 20.0));
         } catch (NoSuchFieldException | IllegalAccessException  e) {
             e.printStackTrace();
             return "";
@@ -106,4 +124,43 @@ public class Imperatrix extends PlaceholderExpansion {
     private double[] getTPS(Object server) throws IllegalAccessException, NoSuchFieldException {
         return (double[]) server.getClass().getField("recentTps").get(server);
     }
+    /**
+     * Find permission prefix and add imput prefix to it.
+     * 
+     * @author LazyGon
+     * @since 1.0.0-SNAPSHOT
+     * 
+     * @param player PLAYER.
+     * @param identifier 
+     * 
+     */
+    public String getPrefix(Player player, String identifier){
+
+        String Prefix = identifier.substring(12);
+        String PermPrefix = "&a*&r";
+
+        Map<String, String> pairs = new HashMap<String, String>();
+
+        String SelectPerm = "select.gradeprefix.";
+        String GroupPerm = "group.";
+
+        pairs.put(SelectPerm+"default", "&a*&r");
+        pairs.put(SelectPerm+"citizens", "&b*&r");
+        pairs.put(SelectPerm+"mod", "&d#&r");
+        pairs.put(SelectPerm+"mod2", "&d&l#&r");
+        pairs.put(SelectPerm+"vip", "&e*&r");
+        pairs.put(GroupPerm+"default", "&a*&r");
+        pairs.put(GroupPerm+"citizens", "&b*&r");
+        pairs.put(GroupPerm+"mod", "&d#&r");
+        pairs.put(GroupPerm+"mod2", "&d&l#&r");
+        pairs.put(GroupPerm+"vip", "&e*&r");
+        pairs.put(GroupPerm+"admins", "&6#&r");
+
+        for(Map.Entry<String, String> perm : pairs.entrySet())
+            if(player.hasPermission(perm.getKey()))
+                PermPrefix = perm.getValue();
+
+        return Prefix + PermPrefix;
+    }
+
 }
