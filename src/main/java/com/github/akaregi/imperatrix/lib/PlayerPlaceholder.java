7ee7fb202b37121277aef924f623a4e5ae932f89
akaregi <akg.tachibana@gmail.com>
2019-03-07 02:05:55 +0900

    Removed redundant import

--- src/main/java/com/github/akaregi/imperatrix/lib/PlayerPlaceholder.java
+++ src/main/java/com/github/akaregi/imperatrix/lib/PlayerPlaceholder.java
@@ -2,7 +2,6 @@ package com.github.akaregi.imperatrix.lib;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
